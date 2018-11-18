d15a24d522cb74ecf72c3463457b159b410e26f6
grant smith <grantsmith1256@gmail.com>
2018-11-18 02:42:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,7 +26,7 @@ attr_accessor :total, :discount, :title, :price, :array
   
   def add_item (title, price, quantity = 0)
     array = []
-    array << title
+    array << title.(quantity.times)
     if quantity == 0
       @total = total + price
     else quantity > 0
@@ -44,7 +44,7 @@ attr_accessor :total, :discount, :title, :price, :array
   end
   
   def items
-    
+
     end
     array
   end

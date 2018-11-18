3820231294209d71bcac52ac0ada9089e8b0a8df
grant smith <grantsmith1256@gmail.com>
2018-11-18 02:08:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ attr_accessor :total, :discount, :title, :price
   def apply_discount
     if @discount > 0
       @total = @total * (@total * (@discount/100))
-      
+      "After the discount, the total comes to $800."
     
     elsif @discount = 0
     "There is no discount to apply."

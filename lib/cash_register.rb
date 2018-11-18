6550e50e32f7bc2bc0aac3f176a467ec5ebd1ed3
grant smith <grantsmith1256@gmail.com>
2018-11-18 01:51:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ attr_accessor :total, :discount, :title, :price
   end
   
   def apply_discount
-    @total = total - (total * (@discount/100))
+    @total = @total - (@total * (@discount/100))
     puts "After the discount, the total comes to #@{total}."
   end
     
